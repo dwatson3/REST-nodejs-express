dc81f739629bcfd76d6ab5775f1f7410f7b1d7b8
Daphne Watson <balancedbody@Daphnes-MacBook-Pro.local>
2018-05-08 16:21:03 -0700

    added middleware into app.js, honestly don't understand much of this section

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 	mongoose = require('mongoose');
+	bodyParser = require('body-parser');
 
 var db = mongoose.connect('mongodb://localhost/bookAPI');
 
@@ -11,9 +12,21 @@ var app = express();
 // creating a port to view this application on
 var port = process.env.PORT || 3000;
 
+app.use(bodyParser.urlencoded({extended:true})); 
+app.use(bodyParser.json()); // returns middleware that only parses JSON
+
 var bookRouter = express.Router();
 
 bookRouter.route('/Books')
+	.post(function(req, res) { // sending a POST request 
+		var book = new Book(req.body);
+
+		// console.log(book);
+		book.save();				// saving the Book
+		// res.send(book);
+		res.status(201).send(book); // sending back the 201 status to let us know that the Book was created
+	})
+
 	.get(function(req, res) {
 		// var responseJson = {hello: "This is my API"};
 
@@ -33,20 +46,45 @@ bookRouter.route('/Books')
 		});
 	})
 
+bookRouter.use('/:bookId', function(req, res, next) {
+	Book.findById(req.params.bookId, function(err, book) {
+		if(err) 
+			res.status(500).send(err);
+		
+		else if(book) {
+			req.book = book;
+			next();
+		
+		} else {
+			res.status(404).send('No book found');
+		}
+	});
+})	
+
 bookRouter.route('/Books/:bookId') //accessing the API by a single query Book Id
 	.get(function(req, res) {
+		res.json(req.book);
+	})
 
+	.put(function(req, res) {	// accessing the API by put, to add new Book information and save it
 		Book.findById(req.params.bookId, function(err, book) {
-			if(err) 
+			if(err)
 				res.status(500).send(err);
-			  else 
+			else
+				book.title = req.body.title;
+				book.author = req.body.author;
+				book.genre = req.body.genre;
+				book.read = req.body.read;
+				book.save();	// saving the new Book information
 				res.json(book);
+				res.json(req.book);
 		});
-	});
+		return bookRouter;
+	})
 
 
-app.use('/api', bookRouter);
-
+app.use('/api/books', bookRouter);
+// app.use('/api/authors', authorRouter); // added an authorRouter to be able to access API by Author Name
 
 
 
